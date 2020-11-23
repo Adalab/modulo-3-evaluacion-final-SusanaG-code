79f7ed6de15a15b143bb43507281fecbdd6dfed1
Susana <su.gonzalez.vejo@gmail.com>
2020-11-23 16:37:13 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+const App = (props) => {
+  return (
+    <body className="body">
+      <header>
+        <h1 className="App">Hola Mundo</h1>
+      </header>
+      <main></main>
+    </body>
+  );
+};
+App.propTypes = {};
+export default App;
