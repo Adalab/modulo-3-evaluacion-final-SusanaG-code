d28ca7a3b8aa20ccfb2f9e559b4f4265899a2136
Susana <su.gonzalez.vejo@gmail.com>
2020-12-08 15:51:21 +0100

    Add fav button

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -6,11 +6,11 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
 const CharacterCard = (props) => {
-  // const handleFav = (ev) => {
-  //   const clickFav = ev.currentTarget.value;
-  //   props.handleFav(clickFav);
-  //   console.log(clickFav);
-  // };
+  const handleFav = (ev) => {
+    const clickFav = ev.currentTarget.value;
+    props.handleFav(clickFav);
+    console.log(clickFav);
+  };
 
   const [color, setColor] = useState("");
 
